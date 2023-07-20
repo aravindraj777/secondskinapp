7807aa092a1a4b57537218e5c3e3a6e2a4f2deb8
Aravind <aravindmunna240@gmail.com>
2023-07-20 10:45:42 +0530

    Initial commit

---
+++ clothes/src/main/java/com/secondskin/clothes/SecondSkinApplication.java
@@ -0,0 +1,13 @@
+package com.secondskin.clothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSkinApplication.class, args);
+	}
+
+}
---
+++ clothes/src/main/java/com/secondskin/clothes/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.secondskin.clothes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home(){
+        return "index";
+    }
+
+}
